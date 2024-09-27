5d28c3c4c69e0a1cd746ee1dbc61f792d6a1a58e
salmon3g <salmon3g@gmail.com>
2024-09-27 16:32:20 +0800

    111

--- src/config.js
+++ src/config.js
@@ -1,16 +1,14 @@
 export const PandaConfig = {
     title: 'My Paper',
-    description: 'Panda Blog, a blog powered by Astro',
-    start: '2016',
+    description: '',
+    start: '2024',
     site: 'https://mypaper.vercel.app/',
     defaultLocale: 'en',
-    navbar: [
+    // navbar: [
         // {title: 'Posts', url: '/'}, // auto generated
-        { title: 'about', url: '/about/' }
-    ],
+        // { title: 'about', url: '/about/' }
+    // ],
     footer: [
-        { title: 'rss', url: '/rss.xml/' },
-        { title: 'contact', url: 'https://github.com/yuhangch/astro-theme-panda/issues/new' },
-        { title: 'github', url: 'https://github.com/yuhangch/astro-theme-panda' }
+        { title: 'rss', url: '/rss.xml/' }
     ]
 }
