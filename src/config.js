5ce3ab7e0bdb511be7115d61cccefac0903a949e
salmon3g <salmon3g@gmail.com>
2024-09-27 19:05:26 +0800

    1

--- src/config.js
+++ src/config.js
@@ -1,9 +1,14 @@
 export const PandaConfig = {
-    title: 'MyPaper',
+    title: 'Panda Blog',
+    description: 'Panda Blog, a blog powered by Astro',
+    start: '2016',
     site: 'https://mypaper.vercel.app/',
     defaultLocale: 'en',
     navbar: [
         // {title: 'Posts', url: '/'}, // auto generated
         { title: 'about', url: '/about/' }
+    ],
+    footer: [
+        { title: 'rss', url: '/rss.xml/' }
     ]
 }
